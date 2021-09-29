355e480b7547c49046cda711e50a58a34c07b525
Mateus Melo <mateus.melo@webjump.com.br>
2021-09-29 16:46:51 -0300

    Upgrading the project until Course Module #6

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,14 +3,39 @@ print('Welcome to Guess The Number')
 print('================================')
 
 num = 32
+total_attempts = 3
+attempt_initial = 1
 
-attempt = int(input('Type your number: '))
 
-print('\nYou typed', attempt)
 
-if (attempt == num):
-    print('Right answer!!!')
-else:
-    print('Wrong...try again!\n')
+for rounds in range(attempt_initial, total_attempts + 1):
+    print('Attempt number: {} of {}  \n'.format(rounds, total_attempts))
+
+    attempt = int(input('Type a number between 1 and 100: '))
+    print('\nYou typed', attempt)
+
+    if (attempt < 1 or attempt > 100):
+        print('Type a number between 1 and 100...try again!\n')
+        print('================================')
+        continue
+
+    right = attempt == num
+    higher = attempt > num
+    lower = attempt < num
+
+    if (right):
+        print('Right answer!!! :D\n')
+        print('================================')
+        break
+    else:
+        if(higher):
+            print('Wrong...you should try a LOWER number...lets go!\n')
+        elif(lower):
+            # It could be 'else'
+            print('Wrong...You should try a HIGHER number...lets go!\n')
+    
+    print('================================')
+    
+    # attempt_initial = attempt_initial + 1
 
 print('End of Game')
